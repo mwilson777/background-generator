7e89a50c705e43609e77390c8867d12667605471
Mike Wilson <mwilson@mindtrust.com>
2025-04-18 13:06:14 -0400

    updated script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
 
 function setGradient() {
-	body.style.background = 
+	body.style.background = `linear-gradient(to right, ${color1}.value)`
 	"linear-gradient(to right, " 
 	+ color1.value 
 	+ ", " 
