24563df6bb0b6adc74745c813b7b79052f3dd4f0
Mike Wilson <mwilson@mindtrust.com>
2025-04-20 23:06:53 -0400

    added new features

--- script.js
+++ script.js
@@ -3,17 +3,35 @@ const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
 
-function setGradient() {
-	body.style.background = `linear-gradient(to right, ${color1}.value)`
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
-
+const setGradient = () => {
+	body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
 	css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+setGradient();
+
+function getRandomColor() {
+	const letters = "0123456789ABCDEF";
+	let color = "#";
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+document.getElementById("randomBtn").addEventListener("click", () => {
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+	setGradient();
+});
+
+document.getElementById("copyBtn").addEventListener("click", () => {
+	const cssText = css.textContent;
+	navigator.clipboard.writeText(cssText)
+		.then(() => alert("CSS copied to clipboard!"))
+		.catch(err => alert("Failed to copy CSS: " + err));
+});
