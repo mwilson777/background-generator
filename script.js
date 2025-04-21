df3b6e999bacbaf00fe44b0d44f154f2542de0db
Mike Wilson <mwilson@mindtrust.com>
2025-04-20 23:24:40 -0400

    cleaned up script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
+const randomBtn = document.getElementById("randomBtn");
+const copyBtn = document.getElementById("copyBtn");
 
 const setGradient = () => {
 	body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
@@ -14,7 +16,7 @@ color2.addEventListener("input", setGradient);
 
 setGradient();
 
-function getRandomColor() {
+const getRandomColor = () => {
 	const letters = "0123456789ABCDEF";
 	let color = "#";
 	for (let i = 0; i < 6; i++) {
@@ -23,13 +25,13 @@ function getRandomColor() {
 	return color;
 }
 
-document.getElementById("randomBtn").addEventListener("click", () => {
+randomBtn.addEventListener("click", () => {
 	color1.value = getRandomColor();
 	color2.value = getRandomColor();
 	setGradient();
 });
 
-document.getElementById("copyBtn").addEventListener("click", () => {
+copyBtn.addEventListener("click", () => {
 	const cssText = css.textContent;
 	navigator.clipboard.writeText(cssText)
 		.then(() => alert("CSS copied to clipboard!"))
